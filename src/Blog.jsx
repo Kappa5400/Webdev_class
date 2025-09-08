8db8df9b892f822b7112fa1de131b5cb4ed4d0a0
Kappa5400 <kappa6708@gmail.com>
2025-09-08 01:18:22 +0000

    chore: testing ci

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -22,7 +22,7 @@ export function Blog() {
       <hr />
       Filter by:{' '}
       <PostFilter
-        field='author'
+        field='author_test'
         value={author}
         onChange={(value) => setAuthor(value)}
       />
