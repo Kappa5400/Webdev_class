87ba0eda0b1f4e6226995fdf9ddec81a8c6d6d6b
Kappa5400 <kappa6708@gmail.com>
2025-09-16 23:58:00 +0000

    chore: mod 6 start

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -10,36 +10,22 @@ export function Blog() {
   const [author, setAuthor] = useState('')
   const [sortBy, setSortBy] = useState('createdAt')
   const [sortOrder, setSortOrder] = useState('descending')
-
   const postsQuery = useQuery({
     queryKey: ['posts', { author, sortBy, sortOrder }],
     queryFn: () => getPosts({ author, sortBy, sortOrder }),
   })
-
   const posts = postsQuery.data ?? []
-
   return (
     <div style={{ padding: 8 }}>
       <Header />
       <hr style={{ margin: "16px 0" }} />
       <h1>Welcome to My Blog!</h1>
-
       <CreatePost />
-
       <PostFilter
         field="author"
         value={author}
-        onChange={setAuthor}
+        onChange={(value) => setAuthor(value)}
       />
-
-      <PostSorting
-        sortBy={sortBy}
-        sortOrder={sortOrder}
-        onChangeSortBy={setSortBy}
-        onChangeSortOrder={setSortOrder}
-      />
-
-      <PostList posts={posts} />
     </div>
   )
 }
